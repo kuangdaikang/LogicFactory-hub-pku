9e5d154b8538ce64e40a70e1153b878e032b3b37
nlwmode <nlwmode@gmail.com>
2024-05-30 17:22:20 +0800

    add cmakelist

---
+++ src/layer_logic/layer_logic/op/po_blasting.hpp
@@ -0,0 +1,79 @@
+#pragma once
+#include "mockturtle/networks/gtg.hpp"
+#include "mockturtle/networks/mig.hpp"
+#include "mockturtle/networks/ntk_src.hpp"
+#include "mockturtle/networks/xag.hpp"
+#include "mockturtle/networks/xmg.hpp
+
+#include <functional>
+#include <unordered_map>
+#include <vector>
+
+namespace lf
+{
+
+/**
+ * @class po blasting
+ * @brief split the given network into single-output sub-network
+ *
+ * @todo only support AIG now
+ *
+ * @code{.cpp}
+ *      aig_network aig;
+ *      ...\\ operations
+ *      std::vector<aig_network> sub_aigs = po_blasting(aig);
+ */
+template<typename Ntk>
+std::vector<Ntk> po_blasting( Ntk const& ntk_src )
+{
+  Ntk ntk_copy( ntk_src );
+  std::vector<Ntk> po_ntks;
+  using signal_t = Ntk::signal_t;
+  using node_t = Ntk::node_t;
+  using index_t = Ntk::index_t;
+
+  std::function<void( signal_t const& )> visit_cone_rec = [&]( signal_t const& f ) -> void { // recursive lambda
+    node_t n = ntk_copy.get_node( f );
+    ntk_copy.set_visited_at( n, true );
+    ntk_copy.foreach_fanin( n, [&]( signal_t const& c ) {
+      visit_cone_rec( c );
+    } );
+  };
+
+  ntk_copy.foreach_po_with_index( [&]( auto const& po, auto ipo ) {
+    Ntk po_aig;
+    ntk_copy.clear_visited();
+    std::unordered_map<index_t, index_t> old_2_new;
+    visit_cone_rec( po );
+    // create pis
+    ntk_copy.foreach_pi_with_index( [&]( auto const& pi, uint32_t index ) {
+      if ( ntk_copy.visited_at( pi ) )
+      {
+        auto new_pi = po_aig.create_pi();
+        old_2_new[index + ntk_copy.num_constants()] = po_aig.get_index( new_pi );
+      }
+    } );
+
+    // create gates
+    ntk_copy.foreach_gate_with_index( [&]( auto const& n, uint32_t index ) {
+      if ( ntk_copy.visited_at( n ) )
+      {
+        signal_t const& c0 = ntk_copy.fanin0_at( n );
+        signal_t const& c1 = ntk_copy.fanin1_at( n );
+
+        signal_t new_c0 = signal_t( old_2_new[ntk_copy.get_index( c0 )], ntk_copy.is_complemented( c0 ) );
+        signal_t new_c1 = signal_t( old_2_new[ntk_copy.get_index( c1 )], ntk_copy.is_complemented( c1 ) );
+        auto new_gate = po_aig.create_and( new_c0, new_c1 );
+        old_2_new[index + ntk_copy.num_constants() + ntk_copy.num_cis()] = po_aig.get_index( new_gate );
+      }
+    } );
+
+    // create po
+    signal_t new_po = signal_t( old_2_new[ntk_copy.get_index( po )], ntk_copy.is_complemented( po ) );
+    po_aig.create_po( new_po );
+    po_ntks.emplace_back( po_aig );
+  } );
+  return po_ntks;
+}
+
+} // namespace lf
---
+++ src/layer_logic/layer_logic/op/strash.hpp
@@ -0,0 +1,104 @@
+#pragma once
+
+#include "mockturtle/networks/aig.hpp"
+#include "mockturtle/networks/gtg.hpp"
+#include "mockturtle/networks/mig.hpp"
+#include "mockturtle/networks/xag.hpp"
+#include "mockturtle/networks/xmg.hpp
+
+#include <unordered_map>
+
+namespace lf
+{
+
+/**
+ * @class Strash
+ * @brief structure hash of the logic network
+ *
+ * @todo only support AIG now
+ *
+ * @code{.cpp}
+ *      aig_network aig;
+ *      ...\\ operations
+ *      aig_network aig_strashed = strash(aig);
+ */
+template<typename Ntk>
+Ntk strash( Ntk const& ntk )
+{
+  Ntk ntk_res;
+
+  using index_t = typename Ntk::index_t;
+  using node_t = typename Ntk::node_t;
+  using signal_t = typename Ntk::signal_t;
+
+  std::unordered_map<node_t, index_t> nodes_hash;  // used for the structure hash
+  std::unordered_map<index_t, index_t> old_to_new; // {id_in_ntk, id_in_ntk_res}
+
+  // create nodes: constant , pis, gates
+  ntk.foreach_node_with_index( [&]( auto const& n, auto id ) {
+    if ( ntk.is_and( id ) )
+    {
+      signal_t oc0 = ntk.fanin0_at( id );
+      signal_t oc1 = ntk.fanin1_at( id );
+      signal_t nc0 = signal_t( old_to_new[ntk.get_index( oc0 )], ntk.is_complemented( oc0 ) ? 1u : 0u );
+      signal_t nc1 = signal_t( old_to_new[ntk.get_index( oc1 )], ntk.is_complemented( oc1 ) ? 1u : 0u );
+
+      node_t dummy_n;
+      dummy_n.fanins = { nc0, nc1 };
+      if ( nodes_hash.find( dummy_n ) != nodes_hash.end() )
+      { // structure hash find
+        old_to_new[id] = nodes_hash[dummy_n];
+      }
+      else
+      { // structure hash not find, create the new and gates
+        signal_t ns_and = ntk_res.create_and( nc0, nc1 );
+        old_to_new[id] = ntk_res.get_index( ns_and );
+        nodes_hash[dummy_n] = ntk_res.get_index( ns_and ); // insert the new added node
+      }
+    }
+    else if ( ntk.is_ci( id ) )
+    {
+      old_to_new[id] = ntk_res.get_index( ntk_res.create_pi() );
+    }
+    else if ( ntk.is_constant( id ) )
+    {
+      index_t oc0id = ntk.get_index( ntk.get_constant( false ) );
+      index_t oc1id = ntk.get_index( ntk.get_constant( true ) );
+      if ( oc0id != oc1id )
+      {
+        index_t nc0id = ntk.get_index( ntk.get_constant( false ) );
+        index_t nc1id = ntk.get_index( ntk.get_constant( true ) );
+        if ( nc0id != nc1id )
+        {
+          old_to_new[oc0id] = nc0id;
+          old_to_new[oc1id] = nc1id;
+        }
+        else
+        {
+          old_to_new[oc0id] = nc0id;
+          old_to_new[oc1id] = nc0id;
+        }
+      }
+    }
+    else
+    {
+      assert( false );
+    }
+  } );
+
+  // create POs
+  ntk.foreach_po( [&]( auto const& f ) {
+    signal_t nf = signal_t( old_to_new[ntk.get_index( f )], ntk.is_complemented( f ) ? 1u : 0u );
+    ntk_res.create_po( nf );
+  } );
+
+  // create RIs
+  ntk.foreach_ri( [&]( auto const& f ) {
+    signal_t nf = signal_t( old_to_new[ntk.get_index( f )], ntk.is_complemented( f ) ? 1u : 0u );
+    ntk_res.create_ri( nf );
+  } );
+
+  return ntk_res;
+}
+
+} // namespace lf
