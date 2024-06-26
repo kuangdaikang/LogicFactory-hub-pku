4dcfe4698f34bbb4c34d9ff4f70b730355220462
nlwmode <nlwmode@gmail.com>
2024-06-26 09:57:20 +0000

    add genlib and liberty reader

---
+++ src/layer_logic/layer_logic/api/abc/io/read_liberty.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "layer_logic/logic_manager.hpp"
+
+#include <cstdio>
+#include <string>
+
+#include "misc/util/abc_namespaces.h"
+#include "misc/util/abc_global.h"
+#include "base/io/io.h"
+#include "misc/extra/extra.h"
+
+#include "utility/string.hpp"
+
+namespace lf
+{
+
+namespace logic
+{
+
+namespace abc
+{
+
+void read_liberty( LogicManager& manager, std::string file )
+{
+  manager.update_logic( E_ToolLogicType::E_LOGIC_ABC_AIG );
+  auto ntk_ptr = manager.current<babc::Abc_Frame_t*>(); // the the network from shared_ptr
+
+  if ( !lf::utility::endsWith( file, ".lib" ) )
+  {
+    std::cerr << "Unmatched liberty suffix type." << std::endl;
+    assert( false );
+    return;
+  }
+
+  int argc = 2;
+  char** argv = ABC_ALLOC( char*, argc + 1 );
+
+  argv[0] = babc::Extra_UtilStrsav( "read" );
+  argv[1] = const_cast<char*>( file.c_str() );
+
+  babc::IoCommandRead( ntk_ptr, argc, argv );
+}
+
+} // namespace abc
+
+} // namespace logic
+
+} // end namespace lf
