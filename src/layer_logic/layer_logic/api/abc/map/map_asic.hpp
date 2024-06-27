9015679dade23d9498cac64124d60fb9c2d00338
nlwmode <nlwmode@gmail.com>
2024-06-27 07:26:58 +0000

    add abc mapping

---
+++ src/layer_logic/layer_logic/api/abc/map/map_asic.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "layer_logic/logic_manager.hpp"
+
+#include <cstdio>
+
+#include "misc/util/abc_namespaces.h"
+#include "misc/util/abc_global.h"
+#include "base/abci/abc.h"
+
+namespace lf
+{
+
+namespace logic
+{
+
+namespace abc
+{
+
+void map_asic( LogicManager& manager )
+{
+  manager.update_logic( E_ToolLogicType::E_LOGIC_ABC_AIG );
+  auto ntk_ptr = manager.current<babc::Abc_Frame_t*>(); // the the network from shared_ptr
+  int argc = 0;
+  char** argv = nullptr;
+  babc::Abc_CommandMap( ntk_ptr, argc, argv );
+}
+
+} // namespace abc
+
+} // namespace logic
+
+} // end namespace lf
