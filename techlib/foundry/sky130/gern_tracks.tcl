d55a917aea722cfaf2c7053734220f2276b92f41
nlwmode <nlwmode@gmail.com>
2024-07-09 21:57:51 +0800

    add foundry

---
+++ techlib/foundry/sky130/gern_tracks.tcl
@@ -0,0 +1,7 @@
+
+gern_track -layer li1 -x_start 480 -x_step 480 -y_start 370 -y_step 370
+gern_track -layer met1 -x_start 480 -x_step 480 -y_start 370 -y_step 370
+gern_track -layer met2 -x_start 480 -x_step 480 -y_start 370 -y_step 740
+gern_track -layer met3 -x_start 480 -x_step 960 -y_start 370 -y_step 740
+gern_track -layer met4 -x_start 480 -x_step 960 -y_start 1850 -y_step 3700
+gern_track -layer met5 -x_start 480 -x_step 960 -y_start 1850 -y_step 3700
