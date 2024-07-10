a2809036bfefb253af0041455340995f23c5187a
nlwmode <nlwmode@gmail.com>
2024-07-10 10:34:54 +0000

    add write_verilog

---
+++ src/layer_logic/layer_logic/api/abc/io/write_verilog.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "layer_logic/logic_manager.hpp"
+
+#include <cstdio>
+#include <string>
+
+#include "misc/util/abc_namespaces.h"
+#include "misc/util/abc_global.h"
+#include "base/io/io.h"
+#include "misc/extra/extra.h"
+
+#include "utility/string.hpp"
+
+namespace lf
+{
+
+namespace logic
+{
+
+namespace abc
+{
+
+void write_verilog( LogicManager& manager, std::string file )
+{
+  if ( !lf::utility::endsWith( file, ".v" ) )
+  {
+    std::cerr << "Unmatched verilog suffix type." << std::endl;
+    assert( false );
+    return;
+  }
+
+  manager.update_logic( E_ToolLogicType::E_LOGIC_ABC_AIG );
+  auto ntk_ptr = manager.current<babc::Abc_Frame_t*>(); // the the network from shared_ptr
+
+  int argc = 2;
+  char** argv = ABC_ALLOC( char*, argc + 1 );
+
+  argv[0] = babc::Extra_UtilStrsav( "write_verilog" );
+  argv[1] = const_cast<char*>( file.c_str() );
+
+  babc::IoCommandWriteVerilog( ntk_ptr, argc, argv );
+}
+
+} // namespace abc
+
+} // namespace logic
+
+} // end namespace lf
