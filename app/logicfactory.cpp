85c20d15f8e72f00134f1f96ccb32d0f96f4cc15
nlwmode <nlwmode@gmail.com>
2024-07-24 03:22:38 +0000

    add banner

--- app/logicfactory.cpp
+++ app/logicfactory.cpp
@@ -1,5 +1,27 @@
 #include "tcl/start.hpp"
 
+void print_banner()
+{
+  auto print_centered = []( const std::string& text, size_t width ) {
+    size_t padding = ( width - text.length() ) / 2;
+    std::cout << "* " << std::string( padding, ' ' ) << text << std::string( padding, ' ' );
+    if ( text.length() % 2 != 0 )
+    {
+      std::cout << " ";
+    }
+    std::cout << " *" << std::endl;
+  };
+
+  const size_t width = 40;
+  std::string line = std::string( width + 4, '-' );
+
+  std::cout << line << std::endl;
+  print_centered( "Welcome to LogicFactory (Platform)", width );
+  print_centered( "Version 0.1", width );
+  print_centered( "Driving innovation in Logic Synthesis!", width );
+  std::cout << line << std::endl;
+}
+
 int main( int argc, char** argv )
 {
   if ( argc == 1 )
@@ -16,7 +38,7 @@ int main( int argc, char** argv )
       break;
     }
   }
-
+  print_banner();
   lf::tcl::engine_start( argc, argv );
 
   return 0;
