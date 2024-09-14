b84771daaa1b4fca97751edd270f30ef8ca1e408
nlwmode <nlwmode@gmail.com>
2024-09-14 12:55:42 +0000

    add max nodes support

--- src/layer_logic/layer_logic/api/lsils/io/write_dot.hpp
+++ src/layer_logic/layer_logic/api/lsils/io/write_dot.hpp
@@ -69,75 +69,152 @@ void write_dot( const std::string& file )
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_STRASH_AIG:
   {
     lf::logic::lsils::aig_seq_network ntk = lfLmINST->current<lf::logic::lsils::aig_seq_network>();
-    mockturtle::write_dot( ntk, file );
+    if ( ntk.num_gates() > 1000 )
+    {
+      std::cout << "warning, too many gates (> 1000), not writing dot file\n";
+    }
+    else
+    {
+      mockturtle::write_dot( ntk, file );
+    }
     break;
   }
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_LOGIC_OIG:
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_STRASH_OIG:
   {
     lf::logic::lsils::oig_seq_network ntk = lfLmINST->current<lf::logic::lsils::oig_seq_network>();
-    mockturtle::write_dot( ntk, file );
+    if ( ntk.num_gates() > 1000 )
+    {
+      std::cout << "warning, too many gates (> 1000), not writing dot file\n";
+    }
+    else
+    {
+      mockturtle::write_dot( ntk, file );
+    }
     break;
   }
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_LOGIC_AOG:
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_STRASH_AOG:
   {
     lf::logic::lsils::aog_seq_network ntk = lfLmINST->current<lf::logic::lsils::aog_seq_network>();
-    mockturtle::write_dot( ntk, file );
+    if ( ntk.num_gates() > 1000 )
+    {
+      std::cout << "warning, too many gates (> 1000), not writing dot file\n";
+    }
+    else
+    {
+      mockturtle::write_dot( ntk, file );
+    }
     break;
   }
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_LOGIC_XAG:
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_STRASH_XAG:
   {
     lf::logic::lsils::xag_seq_network ntk = lfLmINST->current<lf::logic::lsils::xag_seq_network>();
-    mockturtle::write_dot( ntk, file );
+    if ( ntk.num_gates() > 1000 )
+    {
+      std::cout << "warning, too many gates (> 1000), not writing dot file\n";
+    }
+    else
+    {
+      mockturtle::write_dot( ntk, file );
+    }
     break;
   }
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_LOGIC_XOG:
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_STRASH_XOG:
   {
     lf::logic::lsils::xog_seq_network ntk = lfLmINST->current<lf::logic::lsils::xog_seq_network>();
-    mockturtle::write_dot( ntk, file );
+    if ( ntk.num_gates() > 1000 )
+    {
+      std::cout << "warning, too many gates (> 1000), not writing dot file\n";
+    }
+    else
+    {
+      mockturtle::write_dot( ntk, file );
+    }
     break;
   }
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_LOGIC_XMG:
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_STRASH_XMG:
   {
     lf::logic::lsils::xmg_seq_network ntk = lfLmINST->current<lf::logic::lsils::xmg_seq_network>();
-    mockturtle::write_dot( ntk, file );
+    if ( ntk.num_gates() > 1000 )
+    {
+      std::cout << "warning, too many gates (> 1000), not writing dot file\n";
+    }
+    else
+    {
+      mockturtle::write_dot( ntk, file );
+    }
     break;
   }
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_LOGIC_MIG:
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_STRASH_MIG:
   {
     lf::logic::lsils::mig_seq_network ntk = lfLmINST->current<lf::logic::lsils::mig_seq_network>();
-    mockturtle::write_dot( ntk, file );
+    if ( ntk.num_gates() > 1000 )
+    {
+      std::cout << "warning, too many gates (> 1000), not writing dot file\n";
+    }
+    else
+    {
+      mockturtle::write_dot( ntk, file );
+    }
     break;
   }
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_LOGIC_PRIMARY:
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_STRASH_PRIMARY:
   {
     lf::logic::lsils::primary_seq_network ntk = lfLmINST->current<lf::logic::lsils::primary_seq_network>();
-    mockturtle::write_dot( ntk, file );
+    if ( ntk.num_gates() > 1000 )
+    {
+      std::cout << "warning, too many gates (> 1000), not writing dot file\n";
+    }
+    else
+    {
+      mockturtle::write_dot( ntk, file );
+    }
     break;
   }
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_LOGIC_GTG:
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_STRASH_GTG:
   {
     lf::logic::lsils::gtg_seq_network ntk = lfLmINST->current<lf::logic::lsils::gtg_seq_network>();
-    mockturtle::write_dot( ntk, file );
+    if ( ntk.num_gates() > 1000 )
+    {
+      std::cout << "warning, too many gates (> 1000), not writing dot file\n";
+    }
+    else
+    {
+      mockturtle::write_dot( ntk, file );
+    }
     break;
   }
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_NETLIST_FPGA:
   {
     lf::logic::lsils::klut_seq_network ntk = lfLmINST->current<lf::logic::lsils::klut_seq_network>();
-    mockturtle::write_dot( ntk, file );
+    if ( ntk.num_gates() > 1000 )
+    {
+      std::cout << "warning, too many gates (> 1000), not writing dot file\n";
+    }
+    else
+    {
+      mockturtle::write_dot( ntk, file );
+    }
     break;
   }
   case lf::misc::E_LF_NTK_TYPE::E_LF_NTK_TYPE_LSILS_NETLIST_ASIC:
   {
     lf::logic::lsils::blut_seq_network ntk = lfLmINST->current<lf::logic::lsils::blut_seq_network>();
-    mockturtle::write_dot( ntk, file );
+    if ( ntk.num_gates() > 1000 )
+    {
+      std::cout << "warning, too many gates (> 1000), not writing dot file\n";
+    }
+    else
+    {
+      mockturtle::write_dot( ntk, file );
+    }
     break;
   }
   default:
